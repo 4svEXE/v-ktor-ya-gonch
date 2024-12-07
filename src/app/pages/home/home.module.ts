507cf644d00ff904bb2397fc8e854383b6ee2d77
4svEXE <4sv.exe@gmail.com>
2024-12-07 15:08:53 +0100

    home skeleton

--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -4,6 +4,14 @@ import { HomeComponent } from './home.component';
 import { MatrixCalculatorComponent } from './components/matrix-calculator/matrix-calculator.component';
 import { NgxChartsModule } from '@swimlane/ngx-charts';
 import { ReactiveFormsModule } from '@angular/forms';
+import { HiroComponent } from './components/hiro/hiro.component';
+import { AboutMeComponent } from './components/about-me/about-me.component';
+import { StatisticComponent } from './components/statistic/statistic.component';
+import { AboutMatrixComponent } from './components/about-matrix/about-matrix.component';
+import { MyServicesComponent } from './components/my-services/my-services.component';
+import { EtapsComponent } from './components/etaps/etaps.component';
+import { ReviewsComponent } from './components/reviews/reviews.component';
+import { ContactFormComponent } from './components/contact-form/contact-form.component';
 
 
 
@@ -11,6 +19,14 @@ import { ReactiveFormsModule } from '@angular/forms';
   declarations: [
     HomeComponent,
     MatrixCalculatorComponent,
+    HiroComponent,
+    AboutMeComponent,
+    StatisticComponent,
+    AboutMatrixComponent,
+    MyServicesComponent,
+    EtapsComponent,
+    ReviewsComponent,
+    ContactFormComponent,
   ],
   imports: [
     CommonModule,
